5dd9389fd4ac81fcdb44f8eaadb4fa528153b726
FizeChan <chenfengzhan@qq.com>
2019-09-03 10:14:36 +0800

    添加方法Cache::getNew($driver, array $options = [])用于新建实例以便于单独调用。

--- src/Cache.php
+++ src/Cache.php
@@ -22,9 +22,20 @@ class Cache
     public static function getInstance($driver, array $options = [])
     {
         if (empty(self::$handler)) {
-            $class = '\\' . __NAMESPACE__ . '\\handler\\' . $driver;
-            self::$handler = new $class($options);
+            self::$handler = self::getNew($driver, $options);
         }
         return self::$handler;
+    }
+
+    /**
+     * 新建实例
+     * @param string $driver 使用的实际接口名称
+     * @param array $options 配置项
+     * @return CacheHandler
+     */
+    public static function getNew($driver, array $options = [])
+    {
+        $class = '\\' . __NAMESPACE__ . '\\handler\\' . $driver;
+        return new $class($options);
     }
 }
