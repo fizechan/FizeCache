082cecf9c92bf72e97c3f26e5f97e926ed49d1c6
Fize <chenfengzhan@qq.com>
2022-07-08 09:26:46 +0800

    调试。

--- examples/Pool/saveDeferred.php
+++ examples/Pool/saveDeferred.php
@@ -9,18 +9,18 @@ $config = [
 ];
 new Pool($handler, $config);
 
-$item1 = Pool::getItem('item1');
-$item1->set('value1');
+$item1 = Pool::getItem('item11');
+$item1->set('value11');
 $item1->expiresAfter(1000);
 
 Pool::saveDeferred($item1);
 
-$item = Pool::getItem('item1');
+$item = Pool::getItem('item11');
 var_dump($item);
 var_dump($item->isHit());  // true
 
-$item2 = Pool::getItem('item2');
-$item2->set('value2');
+$item2 = Pool::getItem('item22');
+$item2->set('value22');
 Pool::saveDeferred($item2);
 
 // 未调用 Pool::commit() 方法前这些延迟缓存项都尚未持久化
